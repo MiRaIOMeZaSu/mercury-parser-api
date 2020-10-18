5ad4c782af1e0c5304489c87bb93d6ac21eef2ff
MiRaIOMeZaSu <379297266@qq.com>
2020-10-18 11:04:19 +0800

    get headers from env

--- routes.js
+++ routes.js
@@ -1,12 +1,20 @@
 const Router = require('express').Router;
 const router = new Router();
 const Mercury = require('@postlight/mercury-parser');
-
+const URL = require('url');
 router.route('/').get((req, res) => {
     res.json({
         message: 'Welcome to 🚀mercury-parser-api API! Endpoint: /parser',
     });
 });
+
+function get_domain(url) {
+    url = URL.parse(url);
+    let arr = url.hostname.split(".");
+    arr = ["domain"].concat(arr.slice(-2));
+    let domain = arr.join("_");
+    return domain;
+}
 
 router.route('/parser').get(async (req, res) => {
     let result = { message: 'No URL was provided' };
@@ -16,6 +24,8 @@ router.route('/parser').get(async (req, res) => {
             let headers = new Object();
             if (typeof req.query.headers !== 'undefined') {
                 headers = JSON.parse(req.query.headers);
+            } else if (process.env[get_domain(req.query.url)]) {
+                headers = JSON.parse(process.env[get_domain(req.query.url)]);
             }
             result = await Mercury.parse(req.query.url, {
                 contentType,
